61bd2dc2afd2482301ee776f38e9b02f0122b95f
Vasu Tayal <vasutayal1010@gmail.com>
2025-06-21 00:49:29 +0530

    addComment & getCommentofPost controller

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -168,3 +168,63 @@ export const dislikePost = async (req, res) => {
   } catch (error) {}
 };
 
+export const addComment = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const commentKrneWalaUserKiId = req.id;
+
+    const { text } = req.body;
+
+    const post = await Post.findById(postId);
+
+    if (!text)
+      return res
+        .status(400)
+        .json({ message: "text is required", success: false });
+
+    const comment = await Comment.create({
+      text,
+      author: commentKrneWalaUserKiId,
+      post: postId,
+    });
+
+    await comment.populate({
+      path: "author",
+      select: "username profilePicture",
+    });
+
+    post.comments.push(comment._id);
+    await post.save();
+
+    return res.status(201).json({
+      message: "Comment Added",
+      comment,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCommentsOfPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const comments = await Comment.find({ post: postId }).populate(
+      "author",
+      "username profilePicture"
+    );
+
+    if (!comments)
+      return res
+        .status(404)
+        .json({ message: "No comments found for this post", success: false });
+
+    return res.status(200).json({ success: true, comments });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+      
