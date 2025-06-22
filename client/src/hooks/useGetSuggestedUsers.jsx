d5f6eac35f223e48551c6f87dc7b7ea850194220
Vasu Tayal <vasutayal1010@gmail.com>
2025-06-23 00:27:52 +0530

    useGetSuggestedUsers Hooks created

---
+++ client/src/hooks/useGetSuggestedUsers.jsx
@@ -0,0 +1,26 @@
+import { setSuggestedUsers } from "@/redux/authSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+const useGetSuggestedUsers = () => {
+  const dispatch = useDispatch();
+  const BACKEND_URI = "http//:localhost8000";
+  useEffect(() => {
+    const fetchSuggestedUsers = async () => {
+      try {
+        const res = await axios.get(
+          `${BACKEND_URI}/api/v1/user/suggested`,
+          { withCredentials: true }
+        );
+        if (res.data.success) {
+          dispatch(setSuggestedUsers(res.data.users));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchSuggestedUsers();
+  }, []);
+};
+export default useGetSuggestedUsers;
