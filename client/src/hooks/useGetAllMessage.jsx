b546f4b1dfbad1f0dc8497067cac958e86981dd7
Vasu Tayal <vasutayal1010@gmail.com>
2025-06-23 00:25:38 +0530

    useGetAllMeaasge Hooks created

---
+++ client/src/hooks/useGetAllMessage.jsx
@@ -0,0 +1,28 @@
+import { setMessages } from "@/redux/chatSlice";
+import { setPosts } from "@/redux/postSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const useGetAllMessage = () => {
+  const dispatch = useDispatch();
+  const { selectedUser } = useSelector((store) => store.auth);
+  const BACKEND_URI = "http//:localhost8000";
+  useEffect(() => {
+    const fetchAllMessage = async () => {
+      try {
+        const res = await axios.get(
+          `${BACKEND_URI}/${selectedUser?._id}`,
+          { withCredentials: true }
+        );
+        if (res.data.success) {
+          dispatch(setMessages(res.data.messages));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllMessage();
+  }, [selectedUser]);
+};
+export default useGetAllMessage;
