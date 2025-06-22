2b9ff5c1ac5964f04b266750e4c8e25a0b42c63b
Vasu Tayal <vasutayal1010@gmail.com>
2025-06-23 00:28:46 +0530

    useGetProfile Hooks created

---
+++ client/src/hooks/useGetUserProfile.jsx
@@ -0,0 +1,27 @@
+import { setUserProfile } from "@/redux/authSlice";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+
+const useGetUserProfile = (userId) => {
+  const dispatch = useDispatch();
+  // const [userProfile, setUserProfile] = useState(null);
+  const BACKEND_URI = "http//:localhost8000";
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const res = await axios.get(
+          `${BACKEND_URI}/api/v1/user/${userId}/profile`,
+          { withCredentials: true }
+        );
+        if (res.data.success) {
+          dispatch(setUserProfile(res.data.user));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUserProfile();
+  }, [userId]);
+};
+export default useGetUserProfile;
